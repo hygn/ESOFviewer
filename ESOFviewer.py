b959d9a008179148f6cfc8830c69bf6ac0c92e19
hygn <hygn@localhost.localdomain>
2020-04-22 11:51:50 +0900

    bug resolved, function change

--- ESOFviewer.py
+++ ESOFviewer.py
@@ -30,6 +30,7 @@ while True:
     if end == 0:
         try:
             url = input("url: ")
+            print("input cookies (empty is use saved cookies)")
             JSEEEIONID = input("JSESSIONID: ")
             KHANUSER = input("KHANUSER: ")
             hoc = url.split("//")[1].split(".")[0]
@@ -44,83 +45,117 @@ while True:
 #url =  ""
 #JSEEEIONID = ""
 #KHANUSER = ""
-while True:
-    if end == 1:
-        while True:
-            try:
-                url = input("url: ")
-                hoc = url.split("//")[1].split(".")[0]
+if end == 1:
+    while True:
+        try:
+            url = input("url: ")
+            hoc = url.split("//")[1].split(".")[0]
+            safedrive = input("safemode(y/n): ")
+            if safedrive == "y" or safedrive == "n":
+                pass
+            else:
                 safedrive = input("safemode(y/n): ")
-                if safedrive == "y" or safedrive == "n":
-                    pass
-                else:
-                    safedrive = input("safemode(y/n): ")
+            break
+        except IndexError: 
+            print("please input valid value!")
+try:
+    get = url.strip("https://"+hoc+".ebssw.kr/mypage/userlrn/userLrnView.do?")
+    params = get.split("&")
+    cookie = "KHANUSER=" + KHANUSER + "; JSESSIONID=" + JSEEEIONID 
+    while True:
+        try:
+            f = open("cookies.cfg", "r+")
+            if JSEEEIONID == "" or KHANUSER =="":
+                cookie = f.read()
                 break
-            except IndexError: 
-                print("please input valid value!")
-    try:
-        get = url.strip("https://"+hoc+".ebssw.kr/mypage/userlrn/userLrnView.do?")
-        params = get.split("&")
-        cookie = "KHANUSER=" + KHANUSER + "; JSESSIONID=" + JSEEEIONID 
-        dat = curl(url, "", cookie, False)
-        print("main page loaded")
-        cnts = dat.split('if( headerCntntsTyCode === "')[1].split('"')[0]
-        #next load
-        post_data = {
-         'stepSn': params[1].split("=")[1] ,
-         'sessSn': '' , 
-         'atnlcNo': params[0].split("=")[1] , 
-         'lctreSn': params[2].split("=")[1],
-         'cntntsTyCode' : cnts}
-        postfields = urlencode(post_data)
-        dat = curl("https://"+hoc+".ebssw.kr/mypage/userlrn/userLrnMvpView.do", postfields, cookie, True)
-        print(curl("https://"+hoc+".ebssw.kr/mypage/userlrn/userLrnMvpView.do", postfields, cookie, True))
-        print("sub page loaded")
-        #extract video info    
-        video = dat.split('src":"')[1].split('"')[0]
-        revtime = dat.split('var revivTime = Number( "')[1].split('"')[0]
-        print(revtime)
-        print(video)
-        #getjs
-        get_data = {
-         '_': str(time.time()).split(".")[0]}
-        getfields = urlencode(get_data)
-        curl("https://"+hoc+".ebssw.kr/js/require.js?"+getfields, "", cookie, False)
-        curl("https://"+hoc+".ebssw.kr/js/egovframework/com/ebs/cmmn/common.js?"+getfields, "", cookie, False)
-        print("js loaded")
-        #startsig
-        post_data = {
-         'lctreSn': params[2].split("=")[1],
-         'cntntsUseTyCode' : cnts}
-        postfields = urlencode(post_data)
-        curl("https://"+hoc+".ebssw.kr/esof/cmmn/cntntsUseInsert.do", postfields, cookie, True)
-        print("start packet sent")
-        #getvideo
-        sendnoti("download video? \n please open command prompt")
-        getvid = input("download video? (y/n):")
-        if getvid == "y" or getvid == "n":
-            pass
-        else:
-            getvid = input("download video?(y/n): ")
-        if getvid == "y":
-            wget.download(video.replace("\\", ""), 'out.mp4')
-            print("video downloaded")
-        else:
-            print("skip video download")
-        #studycheck
-        i = 0
-        postfields = urlencode(post_data)
-        rep = int(str(int(revtime)/120).split(".")[0])
-        rem = int(revtime) % 120
-        print(revtime)
-        print(rep)
-        print(rem)
-        if True:
-            while True:
-                if i == 0:
-                    lrnmux = 0
+            else:
+                f.write(cookie)
+                break
+        except:
+            f = open("cookies.cfg", "x")
+            f.close()
+    dat = curl(url, "", cookie, False)
+    print("main page loaded")
+    cnts = dat.split('if( headerCntntsTyCode === "')[1].split('"')[0]
+    #next load
+    post_data = {
+     'stepSn': params[1].split("=")[1] ,
+     'sessSn': '' , 
+     'atnlcNo': params[0].split("=")[1] , 
+     'lctreSn': params[2].split("=")[1],
+     'cntntsTyCode' : cnts}
+    postfields = urlencode(post_data)
+    dat = curl("https://"+hoc+".ebssw.kr/mypage/userlrn/userLrnMvpView.do", postfields, cookie, True)
+    print("sub page loaded")
+    #extract video info    
+    video = dat.split('src":"')[1].split('"')[0]
+    revtime = dat.split('var revivTime = Number( "')[1].split('"')[0]
+    print(revtime)
+    print(video)
+    #getjs
+    get_data = {
+     '_': str(time.time()).split(".")[0]}
+    getfields = urlencode(get_data)
+    curl("https://"+hoc+".ebssw.kr/js/require.js?"+getfields, "", cookie, False)
+    curl("https://"+hoc+".ebssw.kr/js/egovframework/com/ebs/cmmn/common.js?"+getfields, "", cookie, False)
+    print("js loaded")
+    #startsig
+    post_data = {
+     'lctreSn': params[2].split("=")[1],
+     'cntntsUseTyCode' : cnts}
+    postfields = urlencode(post_data)
+    curl("https://"+hoc+".ebssw.kr/esof/cmmn/cntntsUseInsert.do", postfields, cookie, True)
+    print("start packet sent")
+    #getvideo
+    sendnoti("download video? \n please open command prompt")
+    getvid = input("download video? (y/n):")
+    if getvid == "y" or getvid == "n":
+        pass
+    else:
+        getvid = input("download video?(y/n): ")
+    if getvid == "y":
+        wget.download(video.replace("\\", ""), 'out.mp4')
+        print("video downloaded")
+    else:
+        print("skip video download")
+    #studycheck
+    i = 0
+    postfields = urlencode(post_data)
+    rep = int(str(int(revtime)/120).split(".")[0])
+    rem = int(revtime) % 120
+    print(revtime)
+    print(rep)
+    print(rem)
+    if True:
+        while True:
+            if i == 0:
+                lrnmux = 0
+            else:
+                lrnmux = 1
+            post_data = {
+            'stepSn': params[1].split("=")[1] ,
+            'sessSn': '' , 
+            'atnlcNo': params[0].split("=")[1] , 
+            'lctreSn': params[2].split("=")[1],
+            'cntntsTyCode' : cnts,
+            'lctreSeCode' : 'LCTRE',
+            'revivTime' : revtime ,
+            'lastRevivLc' : str(120 * i) ,
+            'lrnTime' : str(120*lrnmux)}
+            postfields = urlencode(post_data)
+            curl("https://"+hoc+".ebssw.kr/mypage/userlrn/lctreLrnSave.do", postfields, cookie, True)
+            print("check packet sent")
+            i = i + 1
+            if i != rep:
+                if safedrive == "y":
+                    time.sleep(120+random.randrange(0,4)-2)
+                if safedrive == "n":
+                    time.sleep(10)
+            if i == rep:
+                if safedrive == "y":
+                    time.sleep(rem)
                 else:
-                    lrnmux = 1
+                    time.sleep(10)
                 post_data = {
                 'stepSn': params[1].split("=")[1] ,
                 'sessSn': '' , 
@@ -129,43 +164,18 @@ while True:
                 'cntntsTyCode' : cnts,
                 'lctreSeCode' : 'LCTRE',
                 'revivTime' : revtime ,
-                'lastRevivLc' : str(120 * i) ,
-                'lrnTime' : str(120*lrnmux)}
+                'lastRevivLc' : str(int(revtime)+random.randrange(0,1)) ,
+                'lrnTime' : str(rem),
+                'endButtonYn' :  'Y'
+                 }
                 postfields = urlencode(post_data)
                 curl("https://"+hoc+".ebssw.kr/mypage/userlrn/lctreLrnSave.do", postfields, cookie, True)
-                print("check packet sent")
-                i = i + 1
-                if i != rep:
-                    if safedrive == "y":
-                        time.sleep(120+random.randrange(0,4)-2)
-                    if safedrive == "n":
-                        time.sleep(10)
-                if i == rep:
-                    if safedrive == "y":
-                        time.sleep(rem)
-                    else:
-                        time.sleep(10)
-                    post_data = {
-                    'stepSn': params[1].split("=")[1] ,
-                    'sessSn': '' , 
-                    'atnlcNo': params[0].split("=")[1] , 
-                    'lctreSn': params[2].split("=")[1],
-                    'cntntsTyCode' : cnts,
-                    'lctreSeCode' : 'LCTRE',
-                    'revivTime' : revtime ,
-                    'lastRevivLc' : str(int(revtime)+random.randrange(0,1)) ,
-                    'lrnTime' : str(rem),
-                    'endButtonYn' :  'Y'
-                     }
-                    postfields = urlencode(post_data)
-                    curl("https://"+hoc+".ebssw.kr/mypage/userlrn/lctreLrnSave.do", postfields, cookie, True)
-                    print("end packet sent")
-                    sendnoti("complete! \n put another URL!")
-                    break
-        end = 1
-    except Exception as error: 
-        print("ERROR!!")
-        print("please report this problem")
-        print(error)
-        input("")
-        break
+                print("end packet sent")
+                sendnoti("complete! \n put another URL!")
+                break
+    end = 1
+except Exception as error: 
+    print("ERROR!!")
+    print("please report this problem")
+    print(error)
+    input("")
