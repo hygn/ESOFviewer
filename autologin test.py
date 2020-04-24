f66727a26f82ddd267264eada61919e64c2459e7
hygn <hygn@localhost.localdomain>
2020-04-24 09:15:34 +0900

    cookie change

--- autologin test.py
+++ autologin test.py
@@ -4,4 +4,6 @@ cjr = str(bc.firefox(domain_name="ebssw.kr")).split(">, <")
 res = [i for i in cjr if "hoc26.ebssw.kr" in i] 
 ck1 = res[0].split(" ")[1].split(" ")[0]
 ck2 = res[1].split(" ")[1].split(" ")[0]
-print(ck1 +", " + ck2)
+ck3 = res[2].split(" ")[1].split(" ")[0]
+ck4 = res[3].split(" ")[1].split(" ")[0]
+print(ck1 +", " + ck2+", " + ck3+", " + ck4)
