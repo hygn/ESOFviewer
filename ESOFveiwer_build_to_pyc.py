a7021fe169468e9375b24f55915efdb86d6cd9c4
SJang1 <djraymond1@naver.com>
2020-04-24 09:58:43 +0900

    Update Scripts

---
+++ ESOFveiwer_build_to_pyc.py
@@ -0,0 +1,2 @@
+import py_compile
+py_compile.compile('ESOFviewer.py')
--- __pycache__/ESOFveiwer.cmd
+++ __pycache__/ESOFveiwer.cmd
@@ -3,7 +3,7 @@ pip install wget
 pip install browser_cookie3
 @echo off
 cls
-python ESOFviewer.pyc
+python ESOFviewer.cpython-38.pyc
 echo.
 echo Press any key to close this script
 pause >nul
